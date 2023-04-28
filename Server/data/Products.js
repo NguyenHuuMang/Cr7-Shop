920fa0989ad0e481fb1b3f38dda3d39c78b0778c
mang nguyen <19t1021136@husc.edu.vn>
2023-04-28 16:04:28 +0700

    first commit

---
+++ Server/data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "Balenciaga Tripple S",
+    image: "/images/6.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Nike Air Force 1",
+    image: "/images/5.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Chelsea Boots",
+    image: "/images/4.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Nike Air Jordan",
+    image: "/images/3.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "MLB NY",
+    image: "/images/2.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Yeezy 350",
+    image: "/images/1.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
---
+++ front-end/src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Balenciaga Tripple S",
+    image: "/images/6.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Nike Air Force 1",
+    image: "/images/5.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Chelsea Boots",
+    image: "/images/4.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Nike Air Jordan",
+    image: "/images/3.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "MLB NY",
+    image: "/images/2.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Yeezy 350",
+    image: "/images/1.png",
+    description:
+      "Sneaker là những đôi giày được thiết kế chủ yếu để phục vụ cho thể thao hoặc các hoạt động khác liên quan đến thể dục, còn được biết đến như là giày điền kinh, giày tennis, giày tập gym, giày thể thao, giày chạy hoặc giày tập. Tuy nhiên ngày nay, loại giày này cũng có thể được hiểu là giày dùng để đi thường ngày. Cái tên sneaker được dùng để miêu tả một loại giày với đế linh hoạt được làm từ cao su hoặc các chất liệu tổng hợp, còn phần trên của giày được làm từ da hoặc vải tổng hợp.",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
